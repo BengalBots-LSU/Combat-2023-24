02f7fabec316c138f569a7eb41eaca1e03a15635
yuki-scribbles <115664957+yuki-scribbles@users.noreply.github.com>
2023-10-27 17:21:30 -0500

    Add files via upload

---
+++ Code/Arcade Drive/arcade_receiver_trina.ino
@@ -0,0 +1,77 @@
+#include <SPI.h>       //used to start communication between Arduinos
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Servo.h>
+
+//Variables
+RF24 radio(7, 8); // CE, CSN
+
+//Constant variables
+const byte address[6] = "00001";
+int leftMotorPin = 2, rightMotorPin = 4, weaponMotor = 6;
+//int leftJoystick = 513, rightJoystickA = 502, rightJoystickB = 503;
+Servo LM1, RM1;
+//change
+int buttonMax = 1023;
+//see DIP switch wizard to configure the pins to the servos
+
+//figure out which direction max pwm goes: ccw or cw
+
+// Max size of this struct is 32 bytes - NRF24L01 buffer limit
+struct Data_Package {
+  int jDirectionL = 0;  //reads the command given from the left joystick
+  int jDirectionR = 0;  //reads the command given from the right joystick
+  int killButton = 0;   //reads the command given from the killswitch
+  int weaponButton = 0; //reads the command given from the weapon button
+};
+Data_Package data; //Create a variable with the above structure
+
+
+void setup() {
+  //radio setup
+  Serial.begin(115200);  // boots serial monitor at ? bps
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+
+  //sabertooth setup
+  LM1.attach(leftMotorPin, 1000, 2000);
+  RM1.attach(rightMotorPin, 1000, 2000);
+
+}
+
+void loop() {
+
+  //checks radio availability and reads the sent data
+  if (radio.available()) {
+    radio.read(&data, sizeof(Data_Package)); // Read the whole data and store it into the 'data' structure
+  }
+
+
+  if(data.weaponButton == 0 && data.killButton == 1){ //checks if the weapon needs to be activated
+    //activate weapon
+    //don't activate kill switch
+  } else if(data.weaponButton == 1 && data.killButton == 0){
+    //activate kill switch
+  } else if(data.weaponButton == 0 && data.killButton == 0){
+    //kill bot and/or turn on bot
+  }
+
+  //obtain joystick values and convert them into the ranges 0 - 180
+  int yAxis = map(data.jDirectionL, 0, buttonMax, 0, 180);
+  int xAxis = map(data.jDirectionR, 0, buttonMax, 0, 180);  
+  
+  //turns the joystick values into left and right speeds
+  int rightSpeed = 180 - (yAxis + xAxis);
+  rightSpeed = constrain(rightMotorSpeed, 0, 180);
+  int leftSpeed = yAxis - xAxis;
+  leftSpeed = constrain(leftMotorSpeed, 0 , 180);
+
+  //give power to the motors from 0 (reverse) to 180 (forward)
+  RM1.write(rightSpeed);
+  LM1.write(leftSpeed);
+  //in ms
+  delay(20);
+
+}
