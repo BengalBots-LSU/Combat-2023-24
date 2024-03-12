c1cea1cbf881137511b002e4cbe33fd7cfd49d33
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2024-03-12 14:16:39 -0500

    Update entireReceiver___arcade_drive.ino

--- Code/Arcade Drive/entireReceiver___arcade_drive.ino
+++ Code/Arcade Drive/entireReceiver___arcade_drive.ino
@@ -18,7 +18,112 @@ RF24 radio(7, 8); // CE, CSN
 //LED pins: green - 5, yellow - 6, 
 //if it is connected to transmitter, one is hardwired, one is TBD
 
+//Constant variables /*
+Authors: Maureen Sanchez, BengalBots team
+Purpose: Create an Arduino combat bot to compete in LSU Bengal Brawl against Senior Design teams
+Objective: Acts as the receiver and acts out the commands that the transmitter has sent to it 
+
+Code References:
+- Moving components https://github.com/maureensanchez99/Arroyo-Seco-Duck-Race-2022
+- Parsing through data https://github.com/maureensanchez99/CaSGC-Internship-2022
+*/
+
+//Libraries needed to use certain modules and functions to make this program work
+#include <SPI.h>       //used to start communication between Arduinos
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Servo.h>
+
+//Variables
+RF24 radio(7, 8); // CE, CSN
+//LED pins: green - 5, yellow - 6, 
+//if it is connected to transmitter, one is hardwired, one is TBD
+
 //Constant variables 
+const byte address[6] = "00001";
+const int leftMotor = 24; //saber motor controller
+const int rightMotor = 25; //saber motor controller
+const int weaponMotor = 26; //talon motor controller
+const int kSwitch = 9; //kill switch 
+const int greenLED = 5;
+const int leftMid = 511;
+const int rightMid = 502;
+Servo motorRight, motorLeft, motorWeapon;
+const int buttonMax = 1023;
+
+//Changing Variables
+int joystick[2] = {0, 0};
+
+// Max size of this struct is 32 bytes - NRF24L01 buffer limit
+struct Data_Package {
+  int jDirectionL = 0;  //reads the command given from the left joystick
+  int jDirectionR = 0;  //reads the command given from the right joystick
+  bool killButton = false;   //reads the command given from the killswitch
+  bool weaponButton = false; //reads the command given from the weapon button
+};
+
+Data_Package data; //Create a variable with the above structure
+
+void moveBot(int x, int y) 
+{
+  motorLeft.write(x > leftMid ? x : buttonMax - x);
+  motorRight.write(y > rightMid ? y : buttonMax - y);
+}
+
+void setup() {
+  Serial.begin(115200);  // boots serial monitor at ? bps
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+
+  motorRight.attach(rightMotor, 1000, 2000);
+  motorLeft.attach(leftMotor, 1000, 2000);
+  motorWeapon.attach(weaponMotor, 1000, 2000);
+  //joystick[0] = map(data.jDirectionR, 0, buttonMax, 0, 180);
+  //joystick[1] = map(data.jDirectionL, 0, buttonMax, 180, 0);
+
+  //motorRight.attach(rightMotor); 
+  //motorLeft.attach(leftMotor);
+}
+
+void loop() {
+  pinMode(greenLED, LOW);
+  while(radio.available()) {
+    radio.read(&data, sizeof(Data_Package)); // Read the whole data and store it into the 'data' structure
+    pinMode(greenLED, HIGH);
+    /*
+    if(data.weaponButton == false && data.killButton == true){ //checks if the weapon needs to be activated 
+      digitalWrite(weaponMotor, HIGH); //activate weapon
+      //don't activate kill switch
+    } else if(data.weaponButton == true && data.killButton == false){ 
+      digitalWrite(data.killButton, HIGH);//activate kill switch
+    } else if(data.weaponButton == false && data.killButton == false){
+      
+    }
+    if(data.killButton == 0){
+      //kill bot and/or turn on bot
+    }*/
+
+
+
+    digitalWrite(weaponMotor, data.killButton ? HIGH : LOW);
+
+    moveBot(data.jDirectionL, data.jDirectionR);
+  
+
+  // testing to make sure the values are being collected accurately 
+  Serial.print("a: ");
+  Serial.print(data.jDirectionL);
+  Serial.print(" b: ");
+  Serial.print(data.jDirectionR);
+  Serial.print(" c: ");
+  Serial.print(data.killButton);
+  Serial.print(" d: ");
+  Serial.println(data.weaponButton);
+  Serial.println(" "); 
+  }
+}
 const byte address[6] = "00001";
 const int leftMotor = 2; //saber motor controller
 const int rightMotor = 3; //saber motor controller
